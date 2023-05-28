e83d6a29c0dad6bbe755ce9c2ed30c398a1f2404
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-05-28 13:34:39 +0530

    redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react" 
+import {Routes, Route} from "react-router-dom"
+import Signup from "./Components/Signup"
+import Profile from "./Components/Profile"
+import { useSelector, useDispatch } from "react-redux"
+import { useNavigate } from "react-router-dom"
+import { setCurrentUser } from "./redux/actions/userActions"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+const App = () =>{
+  const currentUser = useSelector(state => state.currentUser)
+  const accessToken = useSelector(state => state.accessToken)
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  // localstorage
+  useEffect(()=>{
+       const user = localStorage.getItem('user') 
+        const accessToken = localStorage.getItem('accessToken') 
+        if(user && accessToken){
+             const currentUser = JSON.parse(user)
+
+           dispatch(setCurrentUser({
+            currentUser, accessToken
+           }))
+        }
+  }, [dispatch]
+  )
+ 
+
+
+  useEffect(()=>{
+       if(!accessToken && window.location.pathname == "/profile" ){
+              navigate("/signup")
+       }
+       if(accessToken && window.location.pathname == "/signup" ){
+              navigate("/profile")
+       }
+  }, [accessToken, navigate, currentUser]
+  )
+     
+  return(
+    <div>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+
+export default App
